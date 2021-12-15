77052c6108114fa71a33a97f905be77635edd245
eaggon <eaggon@gmail.com>
2021-12-14 22:30:38 -1000

    Update

--- Lee_Eaggon_Assignment2/server.js
+++ Lee_Eaggon_Assignment2/server.js
@@ -11,8 +11,10 @@ let generateInvoice = require('./invoice.js').generateInvoice;
 app.use(myParser.urlencoded({ extended: true }));
 
 
+//this is used to submit a checkout
 app.post('/checkout', function (req, res, next){
     let cart = [];
+    //add each product to the cart
     for(let i = 0; i < products.length; i++)
     {
         let quantity = req.body['quantity_'+i];
@@ -25,12 +27,28 @@ app.post('/checkout', function (req, res, next){
             }
         }
     }
+    //if the cart is empty, redirect to the store with an error message
     if (cart.length === 0) 
     {
         res.redirect(
             encodeURI("products_display.html?error=Your cart is empty")
         );
-    } 
+    }
+    //validate that there is enough currently enough inventory to satisfy quantities in the cart
+    let validator = validateInventory(cart);
+    let inventoryError = validator.inventoryError;
+    let inventoryNames = validator.inventoryNames;
+    cart = validator.cart;
+
+    //if there is not enough inventory, redirect to the store with an error message and the cart to the user keeps their valid purchases
+    if(inventoryError)
+    {
+        let inventoryMessage = "Your order exceeds our inventory for the following items: " + inventoryNames.join(', ');
+        res.redirect(
+            encodeURI("products_display.html?error=" + inventoryMessage + "&invError="+JSON.stringify(cart))
+        );
+    }
+    //if there is enough inventory, redirect to the login page
     else 
     {
         res.redirect(encodeURI("/login.html?cart="+JSON.stringify(cart)));
@@ -38,11 +56,14 @@ app.post('/checkout', function (req, res, next){
 });
 
 
+//this is used to create an invoice
 app.get("/invoice", function (req, res, next) {
     let queryCart = req.query.cart;
     let username = req.query.username;
     let parsedCart,userFound;
+    let isNewUser = req.query.newUser;
 
+    //validate that the username exists and parse the cart from the url
     try
     {
         userFound = findUser(username);
@@ -53,9 +74,11 @@ app.get("/invoice", function (req, res, next) {
         res.redirect(encodeURI("products_display.html?error=Invalid invoice request"));
         return;
     }
-    
+
+    //it there is no cart in the url or it is empty, redirect to the store with an error message
     if (!queryCart || parsedCart.length === 0) 
     {
+        //if the username currently has a cart, delete it, as only a malicious interaction with the app would lead here
         if(userFound)
         {
             delete userFound.cart;
@@ -64,21 +87,52 @@ app.get("/invoice", function (req, res, next) {
             encodeURI("products_display.html?error=Your cart is empty")
         );
     }
+    //if the user was not found, redirect to the login page
     else if (!userFound) 
     {
         res.redirect(encodeURI("login.html?cart="+queryCart));
-    } 
+    }
+    //if the user currently has a cart, different from the one in the query, delete it, as only a malicious interaction with the app would lead here
     else if(userFound.cart !== queryCart)
     {
         delete userFound.cart;
         res.redirect(encodeURI("products_display.html?error=Invalid invoice request"));
     }
-    else 
+    
+    //validate that there is enough currently enough inventory to satisfy quantities in the cart
+    let validator = validateInventory(parsedCart);
+    let inventoryError = validator.inventoryError;
+    let inventoryNames = validator.inventoryNames;
+    parsedCart = validator.cart;
+    
+    //if there is not enough inventory, redirect to the store with an error message and the cart to the user keeps their valid purchases
+    if(inventoryError)
+    {
+        let inventoryMessage = "";
+        //if the user requesting the invoice was new, let them know their account has been created, despite lack of inventory for their cart
+        if(isNewUser)
+        {
+            inventoryMessage += "Your account has been registered successfully. ";
+            inventoryMessage += "However, your order exceeds our inventory for the following items: ";
+            inventoryMessage += inventoryNames.join(', ');
+        }
+        else
+        {
+            inventoryMessage += "Your order exceeds our inventory for the following items: ";
+            inventoryMessage += inventoryNames.join(', ');
+        }
+        delete userFound.cart;
+        res.redirect(
+            encodeURI("products_display.html?error=" + inventoryMessage + "&invError="+JSON.stringify(parsedCart))
+        );
+    }
+    //if there is enough inventory, generate and send the invoice
+    else
     {
         let purchased = [];
-        let parsedCart = JSON.parse(queryCart);
         for (let i = 0; i < parsedCart.length; i++)
         {
+            products[parsedCart[i]['key']].inventory -= parsedCart[i]['quantity'];
             purchased.push({...products[parsedCart[i]['key']],quantity: parsedCart[i]['quantity']});
         }
         let invoice_str = generateInvoice(purchased);
@@ -86,23 +140,26 @@ app.get("/invoice", function (req, res, next) {
         invoice_str += `<h2 style="margin: auto;padding-top: 20px;">Thank you for shopping with us, ${userFound.fullName}</h2>`;
         invoice_str += `<br><div style="text-align: center;"><a href="/products_display.html">Continue shopping</a></div>`;
         res.send(invoice_str);
-    };
+    }
 });
 
 // This processes the login form
 app.post("/process_login", function (request, response, next) {
     let cart = request.query.cart;
-    checkUser(
+    //validate that the username and password represent a valid user
+    getUser(
         request.body["uname"],
         request.body["psw"],
         function (user, auth) {
             if (auth) 
             {
+                //if they are valid, redirect to the invoice page
                 user.cart = cart;
                 response.redirect(encodeURI("/invoice?username="+user.username+"&cart="+cart));
             } 
             else 
             {
+                //if they are not valid, redirect to the login page with an error message
                 response.redirect(
                     encodeURI("/login.html?error=Wrong username or password&cart="+cart)
                 );
@@ -115,14 +172,18 @@ app.post("/process_login", function (request, response, next) {
 app.post("/process_registration", function (request, response, next) {
     let data = request.body;
     let cart = request.query.cart;
+    //validate the user information
     let validator = validateUser(data);
+    //if the information is valid, register the user and redirect to the invoice page
     if (validator.success) 
     {
         let user = validator.user;
         user.cart = cart;
         registerUser(user);
-        response.redirect(encodeURI("/invoice?username="+user.username+"&cart="+cart));
-    } else 
+        response.redirect(encodeURI("/invoice?username="+user.username+"&cart="+cart+"&newUser=1"));
+    } 
+    //if the information is not valid, redirect to the registration page with an error message
+    else 
     {
         response.redirect(
             encodeURI("/registration.html?error=" + validator.message+"&cart="+cart)
@@ -130,6 +191,8 @@ app.post("/process_registration", function (request, response, next) {
     }
 });
 
+
+//this is used to load the products by the front end store
 app.get("/load_products", function (request, response, next) {
     response.json(products);
 });
@@ -137,6 +200,27 @@ app.get("/load_products", function (request, response, next) {
 app.use(express.static("./static"));
 
 
+//takes in the cart and validates if there is enough stock for each item
+let validateInventory = function(cart)
+{
+    let inventoryError = false;
+    let inventoryNames = [];
+    for(let i = 0; i < cart.length; i++)
+    {
+        let quantity = parseInt(cart[i].quantity);
+        let prod = products[cart[i].key];
+        if(quantity > parseInt(prod.inventory))
+        {
+            inventoryError = true;
+            inventoryNames.push(prod.name);
+            cart[i].error = true;
+        }
+    }
+    return {cart: cart, inventoryError: inventoryError, inventoryNames: inventoryNames};
+}
+
+
+//register a user and write it to users.txt
 let registerUser = function (data) {
     delete data.confirmPassword;
     let writingData = [...appUsers];
@@ -150,7 +234,8 @@ let registerUser = function (data) {
     fs.promises.writeFile("users.txt", JSON.stringify(writingData));
 };
 
-//validate the input from the registration form
+
+//validate the input from the registration form and if the input is not valid, create an appropriate error message
 let validateUser = function (data) {
     data.username = data.username.toLowerCase();
     if (data.username.length < 4 || data.username.length > 10) {
@@ -159,7 +244,7 @@ let validateUser = function (data) {
             message: "Username should be between 4 and 10 characters",
         };
     }
-    if (checkUserExists(data.username)) {
+    if (findUser(data.username)) {
         return { success: false, message: "Username already exists" };
     }
     if (data.password.length < 6) {
@@ -190,8 +275,8 @@ let validateUser = function (data) {
 };
 
 
-//checks if a username is already registered
-let checkUser = function (username, password, callback) {
+//used to validate user info on login
+let getUser = function (username, password, callback) {
     if (!username || !password) {
         callback({}, false);
     }
@@ -204,16 +289,8 @@ let checkUser = function (username, password, callback) {
     callback({}, false);
 };
 
-//checks if a username is already registered
-let checkUserExists = function (username) {
-    for (let i = 0; i < appUsers.length; i++) {
-        if (appUsers[i].username == username) {
-            return true;
-        }
-    }
-    return false;
-};
 
+//find a user, given its username
 let findUser = function (username) {
     for (let i = 0; i < appUsers.length; i++) {
         if (appUsers[i].username == username) {
@@ -223,6 +300,8 @@ let findUser = function (username) {
     return false;
 };
 
+
+//validate an email address
 let isEmailValid = function (email) {
     let emailRegex =
         /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
